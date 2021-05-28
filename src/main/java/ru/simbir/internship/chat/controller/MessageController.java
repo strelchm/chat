5b9da2490cd21814ab176352117a849921f115ce
strelchm <strelchm@yandex.ru>
2021-05-28 23:04:31 +0700

    Message controller first steps

---
+++ src/main/java/ru/simbir/internship/chat/controller/MessageController.java
@@ -0,0 +1,66 @@
+package ru.simbir.internship.chat.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.simbir.internship.chat.dto.IdDto;
+import ru.simbir.internship.chat.dto.MessageDto;
+import ru.simbir.internship.chat.service.MessageService;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/messages")
+@Validated
+public class MessageController extends ParentController {
+    private final MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GetMapping
+    public List<MessageDto> getAllMessages() {
+        return messageService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public MessageDto getMessageById(@NotNull(message = NULL_ID_REQUEST_EXCEPTION) @Validated @PathVariable UUID id) {
+        return messageService.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public IdDto createMessage(@NotNull(message = NULL_CREATE_OBJECT_REQUEST_EXCEPTION) @Validated @RequestBody MessageDto dto) {
+        return new IdDto(messageService.add(dto));
+    }
+
+    @PutMapping("/{id}")
+    public MessageDto updateMessage(@NotNull(message = NULL_UPDATE_OBJECT_REQUEST_EXCEPTION) @Validated @RequestBody MessageDto dto) {
+        return messageService.edit(dto);
+    }
+
+    @Deprecated // todo not needed here
+    @PatchMapping("/{id}")
+    public MessageDto patchMessage(@NotNull(message = NULL_ID_REQUEST_EXCEPTION) @Validated @PathVariable UUID id,
+                                   @NotNull(message = NULL_PATCH_OBJECT_REQUEST_EXCEPTION) @Validated @RequestBody MessageDto dto) {
+        MessageDto messageDto = messageService.getById(id);
+
+        if (!messageDto.getText().equals(dto.getText())) {
+            messageDto.setText(dto.getText());
+        }
+
+        return messageService.edit(messageDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public MessageDto delete(@NotNull(message = NULL_ID_REQUEST_EXCEPTION) @Validated @PathVariable UUID id) { // todo - void return type
+        MessageDto messageDto = messageService.getById(id); // todo
+        return messageService.delete(messageDto);
+    }
+}
---
+++ src/main/java/ru/simbir/internship/chat/controller/ParentController.java
@@ -0,0 +1,8 @@
+package ru.simbir.internship.chat.controller;
+
+public class ParentController {
+    static final String NULL_ID_REQUEST_EXCEPTION = "Id cannot be null";
+    static final String NULL_CREATE_OBJECT_REQUEST_EXCEPTION = "Instance that must be created not found in request body";
+    static final String NULL_UPDATE_OBJECT_REQUEST_EXCEPTION = "Instance that must be updated not found in request body";
+    static final String NULL_PATCH_OBJECT_REQUEST_EXCEPTION = "Instance that must be patch not found in request body";
+}
